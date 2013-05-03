a117779e48df7b6c44188acc229da9c3674ac510
ZhouQi <zhouqicf@gmail.com>
2013-05-03 16:02:20 +0800

    target language select

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 var context = "selection",
-    title = "Google Translate";
+    title = "Translate It";
 chrome.contextMenus.create({"title": title, "contexts":[context], "onclick": function(info){
-    var searchLink = "http://translate.google.com/#auto/zh-CN/" + encodeURI(info.selectionText);
+    var searchLink = "http://translate.google.com/#auto/" + (localStorage['translate_it_target_language'] || "zh-CN") + "/" + encodeURI(info.selectionText);
     window.open(searchLink);
 }});
